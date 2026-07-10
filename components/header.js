2e313118c245298dcfeadaf8b27ae5a7fb0117a6
daklo91 <coffeedog91@gmail.com>
2026-07-10 10:46:46 +0200

    Update header

--- components/header.js
+++ components/header.js
@@ -1,3 +1,8 @@
 export function Header(totalFollowers) {
-  return `<header><div><div class="logo">Social Media Dashboard</div> <div class="total-followers">Total Followers: ${totalFollowers}</div></div> Dark Mode</header>`;
+  return `<header>
+            <div class="header-container">
+              <div class="logo">Social Media Dashboard</div>
+              <div class="total-followers">Total Followers: ${totalFollowers}</div>
+            </div> Dark Mode
+          </header>`;
 }
