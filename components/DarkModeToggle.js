25c8adf6f33891fa240f6aa21cb47ef638fca712
daklo91 <coffeedog91@gmail.com>
2026-08-02 18:58:11 +0200

    WIP send to laptop

--- components/DarkModeToggle.js
+++ components/DarkModeToggle.js
@@ -1,24 +1,38 @@
 export function DarkModeToggle() {
-  let darkMode = true; 
+  
+let darkMode = true; 
+
+window.setDarkModeTheme = function(darkMode) {
+  const htmlElement = document.documentElement;
+  if (darkMode === true) {
+    htmlElement.setAttribute("data-theme", "dark");
+  } else {
+    htmlElement.setAttribute("data-theme", "light");
+  }
+}
+
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const currentTheme = prefersDark ? 'dark' : 'light';
+
+setDarkModeTheme(prefersDark);
+
 
 window.toggleDarkMode = function() {
-    darkMode = !darkMode;
-    const darkModeKnob = document.getElementById("dark-mode-knob");
-    const htmlElement = document.documentElement;
-
-    if (darkMode === true) {
-        darkModeKnob.style.marginLeft = "calc(100% - 18px)";
-        htmlElement.setAttribute("data-theme", "dark");
-    } else {
-        darkModeKnob.style.marginLeft = "0%";
-        htmlElement.setAttribute("data-theme", "light");
-    }
+  const darkModeKnob = document.getElementById("dark-mode-knob");
+  darkMode = !darkMode;
+  if (darkMode === true) {
+    setDarkModeTheme(true)
+    darkModeKnob.style.marginLeft = "calc(100% - 18px)";
+  } else {
+        setDarkModeTheme(false)
+    darkModeKnob.style.marginLeft = "0%";
+  }
 };
 
   return `<div class="dark-mode-container">
             <span class="text-preset-5">Dark Mode</span>
             <div onclick="toggleDarkMode()" class="dark-mode-toggler">
-                <div style="margin-left: calc(100% - 18px);" id="dark-mode-knob" class="dark-mode-knob"></div>
+                <div style="${prefersDark ? "margin-left: calc(100% - 18px);" : ""}" id="dark-mode-knob" class="dark-mode-knob"></div>
             </div>
           </div>`;
 }
